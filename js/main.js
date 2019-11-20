7fff5cbacf3ff67f8ee30ae67bbc69284714fa47
elenapayan <elena.payan.glez@gmail.com>
2019-11-20 10:09:03 +0100

    Add js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,28 @@
 "use strict";
 
+const button = document.querySelector(".js-button");
+const input = document.querySelector(".js-input");
 
 
-function getRandomNumber(max) {
+const getRandomNumber = function (max) {
     return Math.ceil(Math.random() * max);
+};
+const randomNumber = getRandomNumber(100);
+console.log(randomNumber);
+
+
+const getAClue = function (event) {
+    event.preventDefault();
+    if (input.value < randomNumber) {
+        document.querySelector(".js-paragraph1").innerHTML = "Pista: Demasiado bajo"
+    }
+    else if (input.value > randomNumber) {
+        document.querySelector(".js-paragraph1").innerHTML = "Pista: Demasiado alto"
+    }
+    else {
+        document.querySelector(".js-paragraph1").innerHTML = "¡Has ganado campeona!"
+    }
 
 }
-console.log(getRandomNumber(100));
+
+button.addEventListener("click", getAClue);
